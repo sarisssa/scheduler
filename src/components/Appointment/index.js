183d013eb52d95366cbe3b4feee9fbe1b85d1863
Michael Chan <mikechansas@gmail.com>
2022-01-20 00:47:35 -0800

    Integrate Header, Show and Empty component into Appointment component.

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
+
 import "components/Appointment/styles.scss"
 
 export default function Appointment(props) {
 
-  return (
-    <article className="appointment"></article>
-  )
+  const { id, time, interview } = props;
 
+  return (
+    <article className="appointment">
+      <Header time={time} />
+      {props.interview ? <Show student={interview.student} interviewer={interview.interviewer} /> : <Empty />}
+    </article>
+  )
 }
