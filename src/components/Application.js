123d99751dd065b074c823297e89149899fb2bb2
Michael Chan <mikechansas@gmail.com>
2022-01-20 14:06:24 -0800

    Refactor Application component to use Axios and fetch data from server instead of using static data.

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,28 +1,10 @@
-import React from "react";
-import { useState } from "react";
+import React, {useState, useEffect } from "react";
+import axios from "axios";
 
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
 
 import "components/Application.scss";
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = [
   {
@@ -90,7 +72,15 @@ const appointmentItem = appointments
 
 
 export default function Application(props) {
+  const [days, setDays] = useState([]);
   const [day, setDay] = useState("Monday");
+
+  useEffect(() => {
+    const url = 'api/days';
+    axios.get(url)
+    .then(res => setDays(res.data))
+    .catch(err => console.log(err))
+  },[])
 
   return (
     <main className="layout">
