76b94f39c84f884f614ecf74b83d33b5577b9ffa
Michael Chan <mikechansas@gmail.com>
2022-01-19 21:16:41 -0800

    Incorporate styling into Interviewer List Item component.

--- src/components/InterviewerListItem.jsx
+++ src/components/InterviewerListItem.jsx
@@ -1,13 +1,16 @@
+import React from 'react';
+import "components/InterviewerListItem.scss";
+
 const InterviewerListItem = () => {
     return (
         <li className="interviewers__item">
-  <img
-    className="interviewers__item-image"
-    src="https://i.imgur.com/LpaY82x.png"
-    alt="Sylvia Palmer"
-  />
-  Sylvia Palmer
-</li>
+            <img
+                className="interviewers__item-image"
+                src="https://i.imgur.com/LpaY82x.png"
+                alt="Sylvia Palmer"
+            />
+            Sylvia Palmer
+        </li>
     )
 }
 
