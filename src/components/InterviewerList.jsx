98dd8fe9b77f1528f67fc5d1c933edc959537ead
Michael Chan <mikechansas@gmail.com>
2022-01-19 22:10:25 -0800

    Refactor Interviewer List component to pass stories.

--- src/components/InterviewerList.jsx
+++ src/components/InterviewerList.jsx
@@ -1,14 +1,29 @@
 import React from "react";
 
+import InterviewerListItem from "components/InterviewerListItem"
+
 import "components/InterviewerList.scss"
 
-const InterviewerList = () => {
+const InterviewerList = (props) => {
+    const { interviewers } = props;
+
+    const interviewerItem = interviewers.map(interviewer => {
+
+        return <InterviewerListItem
+            key={interviewer.id}
+            name={interviewer.name}
+            avatar={interviewer.avatar}
+            selected={interviewer.id === props.interviewer}
+            setInterviewer={(event) => props.setInterviewer(interviewer.id)} 
+        />
+    })
+
     return (
         <section className="interviewers">
             <h4 className="interviewers__header text--light">Interviewer</h4>
-            <ul className="interviewers__list"></ul>
+            <ul className="interviewers__list">{interviewerItem}</ul>
         </section>
-    )
+    );
 }
 
 export default InterviewerList;
