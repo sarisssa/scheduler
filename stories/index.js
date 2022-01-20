20ceaebbf25491d2728878b7d85b58d6f88d28c0
Michael Chan <mikechansas@gmail.com>
2022-01-19 23:28:33 -0800

    Add Show stories to index.js and refactor Show component to pass stories.

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,7 @@ import InterviewerList from 'components/InterviewerList';
 import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header.js"
 import Empty from "components/Appointment/Empty.js"
+import Show from "components/Appointment/Show.js"
 
 
 storiesOf("Button", module)
@@ -147,3 +148,10 @@ storiesOf("InterviewerList", module)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
   .add("Header", () => <Header time="12pm" />)
   .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
+  .add("Show", () => (
+    <Show 
+    interviewer={interviewer.name} 
+    student='Lydia Miller-Jones' 
+    onEdit={action("onEdit")} 
+    onDelete={action('onDelete')} 
+    />))
