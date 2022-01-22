66db8b95f30c9b3bb9de0802a88f225ce3c9c427
Michael Chan <mikechansas@gmail.com>
2022-01-21 22:33:05 -0800

    Implement tidy up.

--- src/components/Appointment/Show.js
+++ src/components/Appointment/Show.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 const Show = (props) => {
+    console.log('SHOW', props);
+    
     return (
         <main className="appointment__card appointment__card--show">
             <section className="appointment__card-left">
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,16 +4,18 @@ import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
 
-import "components/Appointment/styles.scss"
+import "components/Appointment/styles.scss";
 
 export default function Appointment(props) {
 
-  const { id, time, interview } = props;
+  const { time, interview } = props;
+
+  console.log('APPT', interview);
 
   return (
     <article className="appointment">
       <Header time={time} />
-      {props.interview ? <Show student={interview.student} interviewer={interview.interviewer} /> : <Empty />}
+      {interview ? <Show student={interview.student} interviewer={interview.interviewer} /> : <Empty />}
     </article>
   )
 }
