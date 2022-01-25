4e187a28009cf494442b21eea3352605e6c53594
Michael Chan <mikechansas@gmail.com>
2022-01-25 14:00:20 -0800

    Add PropTypes testing into InterviewerList component.

--- src/components/InterviewerList.jsx
+++ src/components/InterviewerList.jsx
@@ -1,21 +1,21 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
 import InterviewerListItem from "components/InterviewerListItem"
 
 import "components/InterviewerList.scss"
 
 const InterviewerList = (props) => {
-    const { interviewers } = props;
+    const { interviewers, onChange } = props;
 
     const interviewerItem = interviewers.map(interviewer => {
-
         return <InterviewerListItem
             key={interviewer.id}
             name={interviewer.name}
             avatar={interviewer.avatar}
             selected={interviewer.id === props.value}
             interviewer={interviewer.id}
-            setInterviewer={props.onChange}
+            setInterviewer={onChange}
         />
     })
 
@@ -26,5 +26,9 @@ const InterviewerList = (props) => {
         </section>
     );
 }
+
+InterviewerList.propTypes = {
+    interviewers: PropTypes.array.isRequired
+};
 
 export default InterviewerList;
